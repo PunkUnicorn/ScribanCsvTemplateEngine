be391ef606a66459059f05437feae4915a0bcbc8
Matthew Cocks <matthew.cocks@blackbox-co.com>
2020-10-04 16:37:07 +0100

    Rename

---
+++ ScribanCsvTemplateEngine/Example/Output/SirLancelot/Subfolder/test4.cs
@@ -0,0 +1,14 @@
+data:
+value1
+    [{key: data_test2_nested1, value: value2}, {key: data_test2_nested2, value: value3}, {key: data_test2_nested3, value: value4}, {key: data_test2_nested4, value: [value5, value6]}]
+    [{key: data_test3_nested1, value: [[{key: data_test3_nested1_1, value: value7}, {key: data_test3_nested1_2, value: value8}, {key: data_test3_nested1_3, value: value9}, {key: data_test3_nested1_4, value: value10}], [{key: data_test3_nested1_1, value: value11}, {key: data_test3_nested1_2, value: value12}, {key: data_test3_nested1_3, value: value13}, {key: data_test3_nested1_4, value: value14}]]}, {key: data_test3_nested2, value: [{key: data_test3_nested2_1, value: [{key: data_test3_nested2_1_1, value: [{key: data_test3_nested2_1_1_1, value: [{key: data_test3_nested2_1_1_1_1, value: value16}]}]}]}]}]
+    
+    
+
+
+
+NAME Sir Lancelot
+
+QUEST To find the holy grail
+
+FAV COLOUR Blue
